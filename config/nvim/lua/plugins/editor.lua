406ba4a3f54c7b8875d6b9397d3a824324629494
Ame <47926897+Landreau@users.noreply.github.com>
2026-05-17 11:44:34 +0200

    Update nvim-treesitter setup function

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -17,15 +17,15 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = { "c", "cpp", "rust", "lua", "vim", "vimdoc", "python", "java" },
-        auto_install = true,
-        highlight = { 
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-      })
+    require('nvim-treesitter').setup({
+    ensure_installed = { "c", "cpp", "rust", "lua", "vim", "vimdoc", "python", "java" },
+    auto_install = true,
+    highlight = { 
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = { enable = true },
+    })
     end,
   },
 
